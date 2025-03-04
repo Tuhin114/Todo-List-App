a4303d15f7c2a26d8a9fd21ced5b99ea7d3d4bc8
Tuhin Poddar <tuhinpoddar114@gmail.com>
2025-03-04 18:21:07 +0530

    google font

--- app/index.jsx
+++ app/index.jsx
@@ -9,11 +9,21 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
 import { data } from "@/data/todos";
 
 export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
   const [text, setText] = useState("");
+
+  const [loaded, error] = useFonts({
+    Inter_500Medium,
+  });
+
+  if (!loaded && !error) {
+    return null;
+  }
 
   const addTodo = () => {
     if (text.trim()) {
@@ -101,6 +111,7 @@ const styles = StyleSheet.create({
     padding: 10,
     marginRight: 10,
     fontSize: 18,
+    fontFamily: "Inter_500Medium",
     minWidth: 0,
     color: "white",
   },
@@ -129,6 +140,7 @@ const styles = StyleSheet.create({
   todoText: {
     flex: 1,
     fontSize: 18,
+    fontFamily: "Inter_500Medium",
     color: "white",
   },
   completedText: {
